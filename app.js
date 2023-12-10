b9c57a77039dfe94f2b4665b4327d8abbf156949
Bura Rithvik Kumar <105736916+rithvik4@users.noreply.github.com>
2023-12-10 00:43:38 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
